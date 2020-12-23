bf3d84fe3ee0ea83c1c232b6df8dc679797a8125
Abdou Aziz BA <azizba@groupeidyal.com>
2020-12-23 16:22:11 +0000

    premiere commit pour l'examen

---
+++ src/main/java/com/m1tdsi/examenjava/ExamenJavaApplication.java
@@ -0,0 +1,13 @@
+package com.m1tdsi.examenjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m1tdsi/examenjava/ExamenJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1tdsi.examenjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
